77995e8978375ffe0a95898370a8325ce233aeec
Pavan Katepalli <pavankatepalli@Pavans-MBP-2.home>
2016-06-24 20:06:45 -0400

    conditional styles in react

--- client/resolutions/ResolutionSingle.jsx
+++ client/resolutions/ResolutionSingle.jsx
@@ -11,12 +11,19 @@ export default class ResolutionSingle extends Component {
   }
 
   render(){
+    const resolutionClass = this.props.resolution.complete ? "checked" : "";
+    //console.log('resolutionClass', resolutionClass); //works
+
+    const status = this.props.resolution.complete ? <span className=
+    "completed">Completed</span> : "";
+
     return (
-      <li id="{this.props.resolution._id}">
+      <li className={resolutionClass} id={this.props.resolution._id}>
         <input type="checkbox" readOnly={true} checked={this.props.resolution.complete} onClick={this.toggleChecked.bind(this)} /> {/* makes sure we're utilizing the correct this */}
         {this.props.resolution.text}
         <br /> {/* need the trailing / to make this work */}
-        {this.props.resolution.complete.toString()}
+      {/* uncomment this to see the status this.props.resolution.complete.toString() */}
+        {status}
         <button className="btn-cancel" onClick={this.deleteResolution.bind(this)}>
         x
         </button>
